ff439102467a25e7581b5b7280a5d16a7d270006
rootyss <jaxent@yandex.ru>
2021-02-05 16:34:03 +0300

    'setEnv'

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
-
 import Command from 'commander';
+
 import fs from 'fs';
 
-const version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version;
-
+const { version } = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 const gendiff = Command;
-
 gendiff
-    .version(`${version}`)
-    .description('Compares two configuration files and shows a difference.');
+  .version(`${version}`)
+  .description('Compares two configuration files and shows a difference.');
 
 gendiff.parse();
-
