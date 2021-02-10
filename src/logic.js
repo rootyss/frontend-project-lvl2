66ee4acbfa09205e00434cc42ea78a809f716be3
rootyss <jaxent@yandex.ru>
2021-02-10 16:16:53 +0300

    'fix_logic'

--- src/logic.js
+++ src/logic.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 const getDiff = (data1, data2) => {
-  const dataKeysSorted = Object.keys({...data1, ...data2}).sort();
+  const dataKeysSorted = Object.keys({ ...data1, ...data2 }).sort();
   const diffDatas = dataKeysSorted.reduce((acc, key) => {
     const value1 = data1[key];
     const value2 = data2[key];
@@ -9,16 +9,13 @@ const getDiff = (data1, data2) => {
       if (data1[key] === data2[key]) {
         return [...acc, ` ${key}: ${value1}`];
       }
-      return [...acc, `-${key}: ${value1}`, `+${key}: ${value2}`]
+      return [...acc, `-${key}: ${value1}`, `+${key}: ${value2}`];
     }
     if (!_.has(data1, key)) {
       return [...acc, `+${key}: ${value2}`];
     }
-    if (!_.has(data2, key)) {
-      return [...acc, `-${key}: ${value1}`];
-    }
+    return [...acc, `-${key}: ${value1}`];
   }, []);
-
   return diffDatas.join('\n');
 };
 
