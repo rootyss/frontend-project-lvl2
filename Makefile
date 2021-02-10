3c5bd4f6ddd4f5057cd427a1cfd1b77a75c5773e
rootyss <jaxent@yandex.ru>
2021-02-10 17:35:54 +0300

    'add_tests'

--- Makefile
+++ Makefile
@@ -6,3 +6,5 @@ lint:
 	npx eslint .
 lintFix:
 	npx eslint . --fix
+test:
+	NODE_OPTIONS=--experimental-vm-modules npx jest
---
+++ __tests__/index.test.js
@@ -0,0 +1,32 @@
+import genDiff from '../src/index.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+
+test('diffJson', () => {
+    const path1 = getFixturePath('file1.json');
+    const path2 = getFixturePath('file2.json');
+
+    const expected = '+a: b\n' +
+        '-abc: cba\n' +
+        '-asdf: undefined1\n' +
+        '+asdf: undefined\n' +
+        '-date: today\n' +
+        '+date: tomorroow\n' +
+        '-follow: false\n' +
+        ' host: hexlet.io\n' +
+        '-key: value\n' +
+        '+key: value42\n' +
+        '-proxy: 123.234.53.22\n' +
+        '-timeout: 50\n' +
+        '+timeout: 20\n' +
+        '+verbose: true\n' +
+        '+z: end';
+
+    expect(genDiff(path1, path2)).toEqual(expected);
+});
