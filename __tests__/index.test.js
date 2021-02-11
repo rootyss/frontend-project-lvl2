114259293fdf1bf028f7bd9d7321732b384df737
rootyss <jaxent@yandex.ru>
2021-02-11 16:32:14 +0300

    'add_tests'

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,22 +11,43 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 test('diffJson', () => {
   const path1 = getFixturePath('file1.json');
   const path2 = getFixturePath('file2.json');
+  const expected = '+a: b\n'
+      + '-abc: cba\n'
+      + '-asdf: undefined1\n'
+      + '+asdf: undefined\n'
+      + '-date: today\n'
+      + '+date: tomorroow\n'
+      + '-follow: false\n'
+      + ' host: hexlet.io\n'
+      + '-key: value\n'
+      + '+key: value42\n'
+      + '-proxy: 123.234.53.22\n'
+      + '-timeout: 50\n'
+      + '+timeout: 20\n'
+      + '+verbose: true\n'
+      + '+z: end';
+  expect(genDiff(path1, path2)).toEqual(expected);
+});
+
+test('diffYml', () => {
+  const path1 = getFixturePath('file1.yml');
+  const path2 = getFixturePath('file2.yml');
 
   const expected = '+a: b\n'
-        + '-abc: cba\n'
-        + '-asdf: undefined1\n'
-        + '+asdf: undefined\n'
-        + '-date: today\n'
-        + '+date: tomorroow\n'
-        + '-follow: false\n'
-        + ' host: hexlet.io\n'
-        + '-key: value\n'
-        + '+key: value42\n'
-        + '-proxy: 123.234.53.22\n'
-        + '-timeout: 50\n'
-        + '+timeout: 20\n'
-        + '+verbose: true\n'
-        + '+z: end';
+      + '-abc: cba\n'
+      + '-asdf: undefined1\n'
+      + '+asdf: undefined\n'
+      + '-date: today\n'
+      + '+date: tomorroow\n'
+      + '-follow: false\n'
+      + ' host: hexlet.io\n'
+      + '-key: value\n'
+      + '+key: value42\n'
+      + '-proxy: 123.234.53.22\n'
+      + '-timeout: 50\n'
+      + '+timeout: 20\n'
+      + '+verbose: true\n'
+      + '+z: end';
 
   expect(genDiff(path1, path2)).toEqual(expected);
 });
