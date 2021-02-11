f2c1a33cf77af7b5bf476094998d8769fd0ecc06
rootyss <jaxent@yandex.ru>
2021-02-11 19:34:14 +0300

    'edit_tests'

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import fs from 'fs';
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
@@ -8,32 +9,16 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const expected = '+a: b\n'
-    + '-abc: cba\n'
-    + '-asdf: undefined1\n'
-    + '+asdf: undefined\n'
-    + '-date: today\n'
-    + '+date: tomorroow\n'
-    + '-follow: false\n'
-    + ' host: hexlet.io\n'
-    + '-key: value\n'
-    + '+key: value42\n'
-    + '-proxy: 123.234.53.22\n'
-    + '-timeout: 50\n'
-    + '+timeout: 20\n'
-    + '+verbose: true\n'
-    + '+z: end';
-
 test('diffJson', () => {
   const path1 = getFixturePath('file1.json');
   const path2 = getFixturePath('file2.json');
-
+  const expected = fs.readFileSync(getFixturePath('res-stylish.txt'), 'utf8');
   expect(genDiff(path1, path2)).toEqual(expected);
 });
 
 test('diffYml', () => {
   const path1 = getFixturePath('file1.yml');
   const path2 = getFixturePath('file2.yml');
-
+  const expected = fs.readFileSync(getFixturePath('res-stylish.txt'), 'utf8');
   expect(genDiff(path1, path2)).toEqual(expected);
 });
