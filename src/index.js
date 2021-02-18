a0c2f323616844e18a11f9c5cefa6c8ff0da51c0
rootyss <jaxent@yandex.ru>
2021-02-18 17:41:24 +0300

    'fix_hexlet_errors'

--- src/index.js
+++ src/index.js
@@ -7,17 +7,29 @@ const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const file2 = parsFile(filepath2);
 
   const iter = (data1, data2) => {
-    const unionKeys = [..._.union(_.keys(data1), _.keys(data2))].sort();
+    const unionKeys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
     return unionKeys
       .map((node) => {
         if (!_.has(data1, node)) {
-          return { name: node, type: 'added', value: data2[node] };
+          return {
+              name: node,
+              type: 'added',
+              value: data2[node]
+          };
         }
         if (!_.has(data2, node)) {
-          return { name: node, type: 'deleted', value: data1[node] };
+          return {
+              name: node,
+              type: 'deleted',
+              value: data1[node]
+          };
         }
         if (_.isObject(data1[node]) && _.isObject(data2[node])) {
-          return { name: node, type: 'nested', children: iter(data1[node], data2[node]) };
+          return {
+              name: node,
+              type: 'nested',
+              children: iter(data1[node], data2[node])
+          };
         }
         if ((typeof data1[node] !== typeof data2[node])
               || (data1[node] !== data2[node])) {
@@ -28,7 +40,11 @@ const genDiff = (filepath1, filepath2, format = 'stylish') => {
             valueAfter: data2[node],
           };
         }
-        return { name: node, type: 'identical', value: data1[node] };
+        return {
+            name: node,
+            type: 'identical',
+            value: data1[node]
+        };
       });
   };
   const difference = iter(file1, file2);
