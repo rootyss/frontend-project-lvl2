522aa6b2311ee6d845c81d6183243cd83513fd01
rootyss <jaxent@yandex.ru>
2021-02-24 15:06:03 +0300

    'fix_temp_constants'

--- src/index.js
+++ src/index.js
@@ -4,22 +4,13 @@ import getObjDifference from './getObjDifference.js';
 import { getFullPath, getFileContent, getFileExt } from './utils.js';
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const fullPath1 = getFullPath(filepath1);
-  const fullPath2 = getFullPath(filepath2);
+  const contentFile1 = getFileContent(getFullPath(filepath1));
+  const contentFile2 = getFileContent(getFullPath(filepath2));
 
-  const contentFile1 = getFileContent(fullPath1);
-  const contentFile2 = getFileContent(fullPath2);
+  const data1 = getParser(getFileExt(filepath1))(contentFile1);
+  const data2 = getParser(getFileExt(filepath2))(contentFile2);
 
-  const extensionFile1 = getFileExt(filepath1);
-  const extensionFile2 = getFileExt(filepath2);
-
-  const parserFile1 = getParser(extensionFile1);
-  const parserFile2 = getParser(extensionFile2);
-
-  const file1 = parserFile1(contentFile1);
-  const file2 = parserFile2(contentFile2);
-
-  const difference = getObjDifference(file1, file2);
+  const difference = getObjDifference(data1, data2);
   const render = getRender(format);
   return render(difference);
 };
